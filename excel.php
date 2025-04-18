0d124791e43e98b211ce962d7b4bb9e1a732061c
aweutt <aweutt@gmail.com>
2025-04-18 22:11:18 +0300

    Create excel.php

---
+++ excel.php
@@ -0,0 +1,11 @@
+<h2>مايكروسوفت Excel</h2>
+<iframe src="https://www.office.com/launch/excel" title="Excel"></iframe>
+
+<table>
+  <thead><tr><th>الأمر</th><th>الوظيفة</th></tr></thead>
+  <tbody>
+    <tr><td>=SUM(A1:A5)</td><td>جمع قيم من A1 إلى A5</td></tr>
+    <tr><td>=AVERAGE(B1:B5)</td><td>حساب المتوسط</td></tr>
+    <tr><td>تنسيق خلايا</td><td>تغيير مظهر الأرقام والنصوص</td></tr>
+  </tbody>
+</table>
