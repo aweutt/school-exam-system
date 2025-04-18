dd31c630348fe433d8a861f977af6686f6f2631a
aweutt <aweutt@gmail.com>
2025-04-18 22:14:10 +0300

    Create kodu.php

---
+++ kodu.php
@@ -0,0 +1,11 @@
+<h2>لعبة Kodu Game Lab</h2>
+<iframe src="https://www.microsoft.com/en-us/download/details.aspx?id=10056" title="Kodu"></iframe>
+
+<table>
+  <thead><tr><th>الأمر</th><th>الوظيفة</th></tr></thead>
+  <tbody>
+    <tr><td>WHEN DO</td><td>برمجة رد فعل للكائن عند حدوث شيء</td></tr>
+    <tr><td>move forward</td><td>تحريك الكائن للأمام</td></tr>
+    <tr><td>score +1</td><td>زيادة نقاط اللاعب</td></tr>
+  </tbody>
+</table>
