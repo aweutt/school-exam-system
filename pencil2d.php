2cdf347b22939e69a36623d4b4e0d3dd45c95525
aweutt <aweutt@gmail.com>
2025-04-18 22:20:50 +0300

    Create pencil2d.php

---
+++ pencil2d.php
@@ -0,0 +1,11 @@
+<h2>برنامج Pencil2D</h2>
+<p>برنامج رسم وتحريك ثنائي الأبعاد (2D Animation).</p>
+
+<table>
+  <thead><tr><th>الأداة</th><th>الشرح</th></tr></thead>
+  <tbody>
+    <tr><td>Brush</td><td>الرسم الحر</td></tr>
+    <tr><td>Onion Skin</td><td>عرض الإطارات السابقة لمساعدة في الحركة</td></tr>
+    <tr><td>Timeline</td><td>تنظيم الإطارات في خط زمني</td></tr>
+  </tbody>
+</table>
