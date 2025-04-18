568370d9ff5e1420539eb3faf3a59ff4d1582289
aweutt <aweutt@gmail.com>
2025-04-18 22:16:06 +0300

    Create powerpoint.php

---
+++ powerpoint.php
@@ -0,0 +1,11 @@
+<h2>برنامج PowerPoint</h2>
+<iframe src="https://www.office.com/launch/powerpoint" title="PowerPoint"></iframe>
+
+<table>
+  <thead><tr><th>الأمر</th><th>الوظيفة</th></tr></thead>
+  <tbody>
+    <tr><td>إدراج شريحة</td><td>إضافة شريحة جديدة</td></tr>
+    <tr><td>إضافة انتقال</td><td>إضافة حركة بين الشرائح</td></tr>
+    <tr><td>تشغيل العرض</td><td>عرض الشرائح من البداية</td></tr>
+  </tbody>
+</table>
