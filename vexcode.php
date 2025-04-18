f201f304e27bd817fa6f48cb99f3238b9fff4068
aweutt <aweutt@gmail.com>
2025-04-18 22:17:09 +0300

    Create vexcode.php

---
+++ vexcode.php
@@ -0,0 +1,11 @@
+<h2>برنامج VEXcode VR</h2>
+<iframe src="https://vr.vex.com/" title="VEXcode VR"></iframe>
+
+<table>
+  <thead><tr><th>الأمر</th><th>الوظيفة</th></tr></thead>
+  <tbody>
+    <tr><td>Drive forward</td><td>تحرك الروبوت للأمام</td></tr>
+    <tr><td>Turn right</td><td>انعطاف الروبوت لليمين</td></tr>
+    <tr><td>Repeat</td><td>تكرار تنفيذ أمر معين</td></tr>
+  </tbody>
+</table>
