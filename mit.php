27fd98d9377aed8eb71bbe33d6bd68dbfb89514c
aweutt <aweutt@gmail.com>
2025-04-18 22:19:34 +0300

    Create mit.php

---
+++ mit.php
@@ -0,0 +1,11 @@
+<h2>برنامج MIT App Inventor</h2>
+<iframe src="https://ai2.appinventor.mit.edu/" title="MIT App"></iframe>
+
+<table>
+  <thead><tr><th>البلوك</th><th>الشرح</th></tr></thead>
+  <tbody>
+    <tr><td>when Button.Click</td><td>تشغيل أمر عند الضغط على زر</td></tr>
+    <tr><td>set Label.Text</td><td>تغيير النص الظاهر على الملصق</td></tr>
+    <tr><td>if then</td><td>تنفيذ أمر بناءً على شرط</td></tr>
+  </tbody>
+</table>
