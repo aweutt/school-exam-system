4765da6a15c1517e6a77658ee6991aa8ad6798f0
aweutt <aweutt@gmail.com>
2025-04-18 22:17:48 +0300

    Create shotcut.php

---
+++ shotcut.php
@@ -0,0 +1,11 @@
+<h2>برنامج Shotcut</h2>
+<p>أداة مجانية لتحرير الفيديو تُستخدم في المرحلة المتوسطة.</p>
+
+<table>
+  <thead><tr><th>الأداة</th><th>الوظيفة</th></tr></thead>
+  <tbody>
+    <tr><td>Cut</td><td>قص جزء من المقطع</td></tr>
+    <tr><td>Transition</td><td>إنشاء انتقال بين مقطعين</td></tr>
+    <tr><td>Export</td><td>تصدير الفيديو بعد التعديل</td></tr>
+  </tbody>
+</table>
