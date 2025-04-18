23b59e7a63fcc6393d4cbca96e7c027fc055bf7a
aweutt <aweutt@gmail.com>
2025-04-18 22:15:21 +0300

    Create word.php

---
+++ word.php
@@ -0,0 +1,12 @@
+<h2>برنامج Word</h2>
+<iframe src="https://www.office.com/launch/word" title="Word"></iframe>
+
+<table>
+  <thead><tr><th>الأمر</th><th>الوظيفة</th></tr></thead>
+  <tbody>
+    <tr><td>Ctrl + B</td><td>جعل الخط عريضًا</td></tr>
+    <tr><td>Ctrl + U</td><td>إضافة خط تحت النص</td></tr>
+    <tr><td>إدراج جدول</td><td>إضافة جدول بصفوف وأعمدة</td></tr>
+    <tr><td>إدراج صورة</td><td>إضافة صورة من الجهاز أو الإنترنت</td></tr>
+  </tbody>
+</table>
