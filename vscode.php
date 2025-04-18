5ae91c7af5afb3fc753522ab1096c5577e021b9b
aweutt <aweutt@gmail.com>
2025-04-18 22:18:26 +0300

    Create vscode.php

---
+++ vscode.php
@@ -0,0 +1,11 @@
+<h2>برنامج Visual Studio Code</h2>
+<p>بيئة برمجة قوية تدعم HTML, CSS, JS وغيرها.</p>
+
+<table>
+  <thead><tr><th>الأمر</th><th>الشرح</th></tr></thead>
+  <tbody>
+    <tr><td>! + Tab</td><td>إنشاء هيكل HTML تلقائي</td></tr>
+    <tr><td>Ctrl + /</td><td>تعليق أو إلغاء تعليق السطر</td></tr>
+    <tr><td>Live Server</td><td>معاينة مباشرة للكود في المتصفح</td></tr>
+  </tbody>
+</table>
