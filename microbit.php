8755155c1cc822e1750a89212addc7d73824da00
aweutt <aweutt@gmail.com>
2025-04-18 22:22:29 +0300

    Create microbit.php

---
+++ microbit.php
@@ -0,0 +1,11 @@
+<h2>برنامج Micro:bit</h2>
+<iframe src="https://makecode.microbit.org/" title="Microbit Editor"></iframe>
+
+<table>
+  <thead><tr><th>الكتلة</th><th>الشرح</th></tr></thead>
+  <tbody>
+    <tr><td>on start</td><td>يبدأ عند تشغيل اللوحة</td></tr>
+    <tr><td>show icon</td><td>عرض رمز مثل قلب أو وجه</td></tr>
+    <tr><td>forever</td><td>تكرار تنفيذ أمر بلا توقف</td></tr>
+  </tbody>
+</table>
