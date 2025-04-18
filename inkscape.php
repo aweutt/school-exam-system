0cf32696ae8c5f932f63c2838465fb7476294d6b
aweutt <aweutt@gmail.com>
2025-04-18 22:23:02 +0300

    Create inkscape.php

---
+++ inkscape.php
@@ -0,0 +1,11 @@
+<h2>برنامج Inkscape</h2>
+<p>رسم وتحرير رسومات فيكتور احترافية.</p>
+
+<table>
+  <thead><tr><th>الأداة</th><th>الشرح</th></tr></thead>
+  <tbody>
+    <tr><td>Bezier Tool</td><td>رسم خطوط منحنية</td></tr>
+    <tr><td>Fill and Stroke</td><td>تلوين العناصر وتحديد حدودها</td></tr>
+    <tr><td>Align</td><td>ترتيب العناصر بالنسبة لبعض</td></tr>
+  </tbody>
+</table>
