9492f6c3e3f2cfd68f969a04b4d120f88353b06f
aweutt <aweutt@gmail.com>
2025-04-18 22:10:39 +0300

    Create tinkercad.php

---
+++ tinkercad.php
@@ -0,0 +1,11 @@
+<h2>برنامج Tinkercad</h2>
+<iframe src="https://www.tinkercad.com/dashboard?type=circuits" title="Tinkercad"></iframe>
+
+<table>
+  <thead><tr><th>الأمر</th><th>الوظيفة</th></tr></thead>
+  <tbody>
+    <tr><td>توصيل الأسلاك</td><td>يربط المكونات على الدائرة</td></tr>
+    <tr><td>تشغيل المحاكاة</td><td>يبدأ تجربة المشروع الإلكتروني</td></tr>
+    <tr><td>اختيار مقاومة</td><td>تحديد المقاومة المناسبة بالدائرة</td></tr>
+  </tbody>
+</table>
